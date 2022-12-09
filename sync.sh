fc591578aeff53e734e3c29a1e64177be5b7c5f9
Gustav Kofoed Clausen <gcl@dixa.com>
2022-12-09 22:11:21 +0100

    feat: add script to sync back to repo

---
+++ sync.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Resolve absolute path for current script
+SOURCE="${BASH_SOURCE[0]}"
+while [[ -L "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+
+# Sync Homebrew dependencies
+echo "👉 Syncing Homebrew dependencies..."
+brew bundle dump --force
+
+echo ""
+echo "👉 Syncing files back to repo..."
+FILE_PATHS=$(find "$DIR" -type f -not -path "$DIR/.git/*" -not -name "*.sh" -printf "%P ")
+for FILE_PATH in $FILE_PATHS; do
+    SOURCE="$HOME/$FILE_PATH"
+    DIST="$DIR/$FILE_PATH"
+    if test -f "$SOURCE"; then
+        cp "$SOURCE" "$DIST"
+    fi
+done
+
+echo ""
+echo "Done! Remember to commit and push the changes 🙂"
