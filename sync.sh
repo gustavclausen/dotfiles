9c3f2bf172c4e93e8c1959c59c018ac6bc6ecdfb
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2023-01-15 16:26:29 +0100

    style: sync script format

--- sync.sh
+++ sync.sh
@@ -5,9 +5,9 @@ set -e
 # Resolve absolute path for current script
 SOURCE="${BASH_SOURCE[0]}"
 while [[ -L "$SOURCE" ]]; do # resolve $SOURCE until the file is no longer a symlink
-    DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 
