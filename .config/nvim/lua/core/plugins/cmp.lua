3bc23c666d312d1590029a46a1ffde85c1420b05
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2023-01-15 13:41:41 +0100

    feat: add autocompletion for vim cmdline

--- .config/nvim/lua/core/plugins/cmp.lua
+++ .config/nvim/lua/core/plugins/cmp.lua
@@ -4,6 +4,7 @@ local M = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
   },
 }
 
@@ -23,6 +24,20 @@ function M.config()
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
     }),
     snippet = {
       expand = function(args)
@@ -40,6 +55,22 @@ function M.config()
         ellipsis_char = "...",
       }),
     },
+  })
+
+  cmp.setup.cmdline("/", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = "buffer" },
+    },
+  })
+
+  cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = "path" },
+    }, {
+      { name = "cmdline" },
+    }),
   })
 end
 
