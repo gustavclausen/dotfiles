f2dd14dffaa1d29ee353e84921db81b9591f2db7
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2023-01-18 10:30:14 +0100

    feat: switch some lsp commands to telescope

--- .config/nvim/lua/core/plugins/lsp/utils.lua
+++ .config/nvim/lua/core/plugins/lsp/utils.lua
@@ -23,21 +23,21 @@ function M.custom_lsp_attach(client, bufnr)
     l = {
       name = "LSP",
       a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code action" },
-      d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Go to definition" },
+      d = { "<cmd>Telescope lsp_definitions<cr>", "Go to definition" },
       D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Go to declaration" },
       e = { "<cmd>Telescope diagnostics bufnr=0<cr>", "Diagnostics" },
       k = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover commands" },
       i = {
-        "<cmd>lua vim.lsp.buf.implementation()<cr>",
+        "<cmd>Telescope lsp_implementations<cr>",
         "Show implementations",
       },
-      r = { "<cmd>lua vim.lsp.buf.references()<cr>", "References" },
+      r = { "<cmd>Telescope lsp_references<cr>", "References" },
       R = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
       l = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Line diagnostics" },
       n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next diagnostic" },
       p = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev diagnostic" },
       q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix diagnostics" },
-      t = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "Type definition" },
+      t = { "<cmd>Telescope lsp_type_definitions<cr>", "Type definition" },
     },
   }, { prefix = "<leader>", mode = "n", default_options })
 end
