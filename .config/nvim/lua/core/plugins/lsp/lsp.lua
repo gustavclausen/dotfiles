1a497f0d0fe1a49fb3043db23d8919128e389317
Gustav Kofoed Clausen <gustav@gustavclausen.com>
2023-01-18 12:42:40 +0100

    feat: add new symbols for lsp diagnostics

--- .config/nvim/lua/core/plugins/lsp/lsp.lua
+++ .config/nvim/lua/core/plugins/lsp/lsp.lua
@@ -5,10 +5,10 @@ local languages = require("core.plugins.lsp.languages")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 local servers = {
