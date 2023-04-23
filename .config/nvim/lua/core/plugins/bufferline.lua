34d9e7d05a0008b544baab29290557b10aadfb52
Gustav Kofoed Clausen <gustavclausen@users.noreply.github.com>
2023-04-23 16:28:46 +0200

    style(nvim): add file-explorer section on bufferline

--- .config/nvim/lua/core/plugins/bufferline.lua
+++ .config/nvim/lua/core/plugins/bufferline.lua
@@ -7,7 +7,18 @@ local M = {
 }
 
 M.config = function()
-  require("bufferline").setup()
+  require("bufferline").setup({
+    options = {
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "File Explorer",
+          highlight = "Directory",
+          separator = true, -- use a "true" to enable the default, or set your own character
+        },
+      },
+    },
+  })
 
   vim.api.nvim_set_keymap("n", "<c-n>", "<cmd>BufferLineCycleNext<CR>", { noremap = true, silent = false })
   vim.api.nvim_set_keymap("n", "<c-p>", "<cmd>BufferLineCyclePrev<CR>", { noremap = true, silent = false })
