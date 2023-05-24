e82ac4cf360f4803363f53441419f6a637b0f418
Gustav Kofoed Clausen <gustavclausen@users.noreply.github.com>
2023-05-24 21:56:55 +0200

    fix(nvim): lsp icons after nerd-fonts upgrade

--- .config/nvim/lua/core/plugins/lsp/lsp.lua
+++ .config/nvim/lua/core/plugins/lsp/lsp.lua
@@ -5,7 +5,7 @@ local languages = require("core.plugins.lsp.languages")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
