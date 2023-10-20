4b5b083ff33d54b85cc584ee056d7a3495019115
Gustav Kofoed Clausen <gustavclausen@users.noreply.github.com>
2023-10-20 08:40:49 +0200

    chore(nvim): update plugins

--- .config/nvim/lua/core/plugins/telescope.lua
+++ .config/nvim/lua/core/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  version = "0.1.0",
+  version = "0.1.4",
   dependencies = {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-lua/plenary.nvim",
